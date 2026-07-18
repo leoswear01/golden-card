a94e4f577f408190768892816a2a714e5e832f5b
Hasith <sarangabizlk@gmail.com>
2026-07-19 04:37:41 +0530

    update

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const SETTINGS = {
 
     minimumPurchase: "LKR 3000",
 
-    couponCode: "JUL9i",
+    couponCode: "JUL9907",
 
     expiry: "31 July 2026",
     description: "Enjoy your exclusive monthly reward specially prepared for our Golden Members.",
